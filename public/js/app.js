d4d2243e97d67e56ad1b2db7295d439376c1d7a0
21Jon <l.jonas.1@web.de>
2020-10-13 18:19:23 +0200

    Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -40,7 +40,7 @@ weatherForm.addEventListener('submit', (e) =>{
     messagetwo.textContent = ''  
     messagetree.textContent = ''
 
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
 
 response.json().then((data)=>{
     if (data.error) {
